943f978cd8040d42a0026afb77eac950ecd37083
Chayan Pal <chayan110201@gmail.com>
2025-01-19 12:06:37 +0530

    ViewModel created.

---
+++ app/src/main/java/com/example/moviz/viewmodel/MovieViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.moviz.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.moviz.repository.Repository
+import com.example.moviz.retrofit.Movie
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.example.moviz.BuildConfig
+import kotlinx.coroutines.launch
+
+class MovieViewModel(repository: Repository): ViewModel() {
+
+    var movies by mutableStateOf<List<Movie>>(emptyList())
+        private set
+
+    var moviesFromAPI by mutableStateOf<List<Movie>>(emptyList())
+        private set
+
+    init {
+        viewModelScope.launch{
+            try {
+
+                val apiKey = BuildConfig.API_KEY
+                moviesFromAPI=repository.getPopularMoviesFromOnlineApi(apiKey)
+
+                movies=moviesFromAPI
+            }catch (e:Exception){
+
+            }
+        }
+    }
+}
