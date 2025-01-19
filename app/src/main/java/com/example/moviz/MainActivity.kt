0021551edcf42f7a7ea2f927a42cbcf5e1607ea6
Chayan Pal <chayan110201@gmail.com>
2025-01-19 12:13:36 +0530

    MovieViewModel and MovieScreen added with ViewModelFactory and Repository.

--- app/src/main/java/com/example/moviz/MainActivity.kt
+++ app/src/main/java/com/example/moviz/MainActivity.kt
@@ -4,44 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.example.moviz.repository.Repository
+import com.example.moviz.screens.MovieScreen
 import com.example.moviz.ui.theme.MovizTheme
+import com.example.moviz.viewmodel.MovieViewModel
+import com.example.moviz.viewmodel.MovieViewModelFactory
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val repository = Repository()
+
+        val viewModelFactory = MovieViewModelFactory(repository)
+
+        val movieViewModel = ViewModelProvider(
+            this, viewModelFactory
+        )[MovieViewModel::class.java]
+
         setContent {
             MovizTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        MovieScreen(viewModel = movieViewModel)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovizTheme {
-        Greeting("Android")
     }
 }
