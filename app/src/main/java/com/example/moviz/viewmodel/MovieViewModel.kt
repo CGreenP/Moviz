f1d5f353d704eae08348db3b483a480403b11ffe
Chayan Pal <chayan110201@gmail.com>
2025-01-21 12:54:24 +0530

    MovieViewModel class implemented, including API data handling and database integration.

--- app/src/main/java/com/example/moviz/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/moviz/viewmodel/MovieViewModel.kt
@@ -1,18 +1,22 @@
 package com.example.moviz.viewmodel
 
-import androidx.lifecycle.ViewModel
-import com.example.moviz.repository.Repository
-import com.example.moviz.retrofit.Movie
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.moviz.BuildConfig
+import com.example.moviz.repository.Repository
+import com.example.moviz.retrofit.Movie
 import kotlinx.coroutines.launch
 
-class MovieViewModel(repository: Repository): ViewModel() {
+class MovieViewModel(repository: Repository) : ViewModel() {
 
     var movies by mutableStateOf<List<Movie>>(emptyList())
+        private set
+
+    var popularMovies by mutableStateOf<List<Movie>>(emptyList())
         private set
 
     var moviesFromAPI by mutableStateOf<List<Movie>>(emptyList())
@@ -23,18 +27,23 @@ class MovieViewModel(repository: Repository): ViewModel() {
 
 
     init {
-        viewModelScope.launch{
+        viewModelScope.launch {
             try {
 
                 val apiKey = BuildConfig.API_KEY
-                moviesFromAPI=repository.getPopularMoviesFromOnlineApi(apiKey)
+                moviesFromAPI = repository.getPopularMoviesFromOnlineApi(apiKey)
 
                 repository.insertMoviesListToDB(moviesFromAPI)
 
-                movies=moviesFromAPI
-            }catch (e:Exception){
-                moviesFromRoomDB=repository.getPopularMoviesFromDB()
-                movies=moviesFromRoomDB
+                movies = moviesFromAPI
+
+                popularMovies = movies.sortedByDescending { it.popularity }
+
+            } catch (e: Exception) {
+                Log.d("TAG", "MovieViewModel: ${e.message}")
+                moviesFromRoomDB = repository.getPopularMoviesFromDB()
+                movies = moviesFromRoomDB
+                popularMovies = movies.sortedByDescending { it.popularity }
             }
         }
     }
