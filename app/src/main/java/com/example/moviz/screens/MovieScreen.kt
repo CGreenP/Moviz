b022d0b450ae3d3999b8aee6103d1996cc19a7f0
Chayan Pal <chayan110201@gmail.com>
2025-01-21 15:11:48 +0530

    Add a loading indicator to the MovieScreen.

--- app/src/main/java/com/example/moviz/screens/MovieScreen.kt
+++ app/src/main/java/com/example/moviz/screens/MovieScreen.kt
@@ -1,10 +1,24 @@
 package com.example.moviz.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.moviz.viewmodel.MovieViewModel
 
 @Composable
-fun MovieScreen(viewModel: MovieViewModel){
+fun MovieScreen(viewModel: MovieViewModel) {
     val moviesList = viewModel.popularMovies
-    MovieList(movies = moviesList)
+    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+    if (isLoading) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    } else {
+        MovieList(movies = moviesList)
+    }
 }
