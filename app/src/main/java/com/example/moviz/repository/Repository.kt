69d8cf9fcd5f62d532d20217fb7d37ead18ff793
Chayan Pal <chayan110201@gmail.com>
2025-01-21 11:48:50 +0530

    Repository class function added to fetch data from ROOM database.

--- app/src/main/java/com/example/moviz/repository/Repository.kt
+++ app/src/main/java/com/example/moviz/repository/Repository.kt
@@ -1,12 +1,31 @@
 package com.example.moviz.repository
 
+import android.content.Context
 import com.example.moviz.retrofit.Movie
 import com.example.moviz.retrofit.RetrofitInstance
+import com.example.moviz.room.MoviesDB
 
-class Repository {
+class Repository(context: Context) {
 
     // Fetching data from Online API
     suspend fun getPopularMoviesFromOnlineApi(apiKey: String): List<Movie> {
         return RetrofitInstance.api.getPopularMovies(apiKey).results
     }
+
+    //Fetching data from Offline ROOM database
+    private val db = MoviesDB.getInstance(context)
+    private val moviesDAO = db.moviesDAO
+
+    suspend fun getPopularMoviesFromDB(): List<Movie> {
+        return moviesDAO.getAllPopularMoviesInDB()
+    }
+
+    suspend fun insertMoviesListToDB(movies: List<Movie>) {
+        return moviesDAO.insertMoviesList(movies)
+    }
+
+    suspend fun insertMovieIntoDB(movie: Movie) {
+        return moviesDAO.insert(movie)
+    }
+
 }
