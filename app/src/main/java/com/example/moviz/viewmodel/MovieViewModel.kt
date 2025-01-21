95c07ab1e529b9098bdaebd1e146f7fa7ee2eaae
Chayan Pal <chayan110201@gmail.com>
2025-01-21 15:12:18 +0530

    Update MovieViewModel with loading state and data refresh.

--- app/src/main/java/com/example/moviz/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/moviz/viewmodel/MovieViewModel.kt
@@ -9,9 +9,20 @@ import androidx.lifecycle.viewModelScope
 import com.example.moviz.BuildConfig
 import com.example.moviz.repository.Repository
 import com.example.moviz.retrofit.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class MovieViewModel(repository: Repository) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.onStart {
+            loadData(repository = repository)
+        }.stateIn(
+            viewModelScope, SharingStarted.WhileSubscribed(5000L), false
+        )
 
     var movies by mutableStateOf<List<Movie>>(emptyList())
         private set
@@ -25,11 +36,10 @@ class MovieViewModel(repository: Repository) : ViewModel() {
     var moviesFromRoomDB by mutableStateOf<List<Movie>>(emptyList())
         private set
 
-
-    init {
+    fun loadData(repository: Repository) {
         viewModelScope.launch {
             try {
-
+                _isLoading.value = true
                 val apiKey = BuildConfig.API_KEY
                 moviesFromAPI = repository.getPopularMoviesFromOnlineApi(apiKey)
 
@@ -38,12 +48,15 @@ class MovieViewModel(repository: Repository) : ViewModel() {
                 movies = moviesFromAPI
 
                 popularMovies = movies.sortedByDescending { it.popularity }
+                _isLoading.value = false
 
             } catch (e: Exception) {
+                _isLoading.value = true
                 Log.d("TAG", "MovieViewModel: ${e.message}")
                 moviesFromRoomDB = repository.getPopularMoviesFromDB()
                 movies = moviesFromRoomDB
                 popularMovies = movies.sortedByDescending { it.popularity }
+                _isLoading.value = false
             }
         }
     }
