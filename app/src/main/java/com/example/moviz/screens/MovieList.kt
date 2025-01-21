7b97ba826aefe6b69a0a61d595a57ac6230f3e02
Chayan Pal <chayan110201@gmail.com>
2025-01-21 16:29:42 +0530

    LazyColumn replaced with LazyVerticalGrid for movie list display.

--- app/src/main/java/com/example/moviz/screens/MovieList.kt
+++ app/src/main/java/com/example/moviz/screens/MovieList.kt
@@ -1,15 +1,21 @@
 package com.example.moviz.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.moviz.retrofit.Movie
 
 @Composable
 fun MovieList(movies: List<Movie>){
 
-    LazyColumn {
-        items(movies, key = {movie -> movie.id}){
+    LazyVerticalGrid(GridCells.Adaptive(400.dp), modifier = Modifier.fillMaxSize()) {
+        items(movies, key = {it.id}){
                 movie -> MovieItem(movie)
         }
     }
